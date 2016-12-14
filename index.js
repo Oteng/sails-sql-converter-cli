ad377e378572c68001c0761bc5bfe8a9a9548faf
oteng <otengkwaku@gmail.com>
2016-12-14 12:06:45 +0000

    adding opt to index.js

--- index.js
+++ index.js
@@ -13,12 +13,67 @@
  * Licensed under the MIT license.
  *
  */
-// var Reader_1 = require("./src/Reader");
-// var Writer_1 = require("./src/Writer");
-// (function main() {
-//     var reader = new Reader_1.Reader("test/db.sql");
-//     reader.read(function (e, r, f) {
-//         // console.log(e, r)
-//         Writer_1.Writer.write(r, f);
-//     });
-// })();
+
+//external imports
+var nopt = require('nopt');
+var path = require('path');
+var fs = require('fs');
+
+//Options
+var knowOpt = {
+    "file": [String, null],
+    "help": Boolean,
+    "out": [String, null]
+};
+var shortHands = {
+    "h": ["--help"],
+    "o":["--out"]
+};
+
+
+var optParsed = nopt(knowOpt, shortHands, process.argv, 2);
+
+//print help message if opt contains help flag or no flag is provided
+if (optParsed.help || Object.keys(optParsed).length < 2) {
+    printHelpMessage();
+}
+
+//for any other thing check if file is provided
+if(optParsed.file && optParsed.file == 'true')
+    printHelpMessage();
+
+//check if path is a valid path and file exist and ends in sql
+var fileDes = path.parse(optParsed.file);
+if(!fs.existsSync(optParsed.file))
+    printHelpMessage("Check if file exist and try again");
+if(fileDes.ext != '.sql')
+    printHelpMessage("Check if file is a valid sql file");
+
+
+//all is fine so far call program
+var reader = require('../src/Reader');
+var writer = require('../src/Writer');
+(function () {
+    var Reader = new reader.Reader(optParsed.file);
+    Reader.read(function (error, tableRoot, foreignKeyRoot) {
+        if(error){
+            console.log(error);
+            process.exit(1);
+        }else{
+            writer.Writer.write(tableRoot, foreignKeyRoot);
+        }
+    })
+})();
+
+
+
+//helper function
+function printHelpMessage(msg) {
+    var help = "Usage: sails-sql -f \"schema_file\" \n" +
+        "       sails-sql -file \"schema_file\" \n" +
+        "-h --help : print this message\n" +
+        "-o --out  : specify a custom output director. Director should exist." +
+        " If sails-sql is ruined without -o it assume the cwd is the root of a valid sails app";
+    console.log((msg)? msg:help);
+    process.exit();
+}
