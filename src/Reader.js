e8cde50a38d42848a8a6a9b2eccb0690c767ad3b
oteng <otengkwaku@gmail.com>
2016-12-15 12:07:29 +0000

    Refactor and renaming variables

--- src/Reader.js
+++ src/Reader.js
@@ -2,10 +2,10 @@
  * Created by oteng on 10/8/16.
  */
 "use strict";
-var Table_1 = require("./Table");
-var Util_1 = require("./Util");
-var Column_1 = require("./Column");
-var ForeignKey_1 = require("./ForeignKey");
+var Table = require("./Table");
+var Util = require("./Util");
+var Column = require("./Column");
+var ForeignKey = require("./ForeignKey");
 var Reader = (function () {
     function Reader(opt) {
         this.reader = require('fs').createReadStream(opt.file, {
@@ -20,8 +20,8 @@ var Reader = (function () {
         var foreign_root = null;
         var char;
         var keyword = '';
-        var maincontext = '';
-        var datacontext = '';
+        var mainContext = '';
+        var dataContext = '';
         var ignore = false;
         var tmpData = '';
         this.reader.on('readable', function () {
@@ -29,7 +29,7 @@ var Reader = (function () {
             var foreignKeyName = '';
             while (null !== (char = _this.reader.read(1))) {
                 someDataRead = true;
-                //read until you mee a space or a line break then breck out of the loop and process what has been learnt
+                //read until you meet a space or a line break then break out of the loop and process what has been learnt
                 switch (char) {
                     case '\n':
                     case ' ':
@@ -40,36 +40,36 @@ var Reader = (function () {
                         }
                         switch (keyword.toLocaleLowerCase()) {
                             case "create":
-                                //set context and init keyword for next keyword
-                                maincontext = 'create';
+                                //set context for next keyword
+                                mainContext = 'create';
                                 // keyword = '';
-                                datacontext = '';
+                                dataContext = '';
                                 ignore = false;
                                 tmpData = '';
                                 break;
                             case "table":
-                                if (maincontext == 'create') {
+                                if (mainContext == 'create') {
                                     if (root == null) {
-                                        root = new Table_1.Table("table");
+                                        root = new Table.Table("table");
                                     }
                                     else {
-                                        var last = Util_1.Util.getLast(root);
-                                        last.setNext(new Table_1.Table("table"));
+                                        var last = Util.Util.getLast(root);
+                                        last.setNext(new Table.Table("table"));
                                     }
                                 }
                                 break;
                             case "primary":
-                                maincontext = 'primary';
+                                mainContext = 'primary';
                                 break;
                             case "foreign":
-                                maincontext = 'foreign';
+                                mainContext = 'foreign';
                                 break;
                             case "references":
-                                maincontext = 'references';
+                                mainContext = 'references';
                                 break;
                             default:
-                                if (datacontext == "column_type") {
-                                    var last = Util_1.Util.getLast(root);
+                                if (dataContext == "column_type") {
+                                    var last = Util.Util.getLast(root);
                                     var tmp = last.getLastColumn();
                                     if (keyword.toLocaleLowerCase() == 'int') {
                                         tmp.setType("integer");
@@ -77,7 +77,7 @@ var Reader = (function () {
                                     else if (keyword.toLocaleLowerCase() == 'varchar' || keyword.toLocaleLowerCase() == 'char') {
                                         tmp.setType("string");
                                     }
-                                    datacontext = '';
+                                    dataContext = '';
                                 }
                         }
                         keyword = '';
@@ -85,26 +85,26 @@ var Reader = (function () {
                     case '`':
                         //beginning of data or end
                         //set context so we know if we are beginning or ending
-                        if (datacontext == "data_end") {
-                            if (tmpData == "__ac" && maincontext == "create") {
-                                var last = Util_1.Util.getLast(root);
+                        if (dataContext == "data_end") {
+                            if (tmpData == "__ac" && mainContext == "create") {
+                                var last = Util.Util.getLast(root);
                                 last.setTableName(keyword);
                                 keyword = '';
-                                datacontext = '';
+                                dataContext = '';
                                 tmpData = '';
                             }
-                            else if (maincontext == "columns") {
-                                var last = Util_1.Util.getLast(root);
-                                last.addColumnObj(new Column_1.Column(keyword));
-                                datacontext = "column_type";
+                            else if (mainContext == "columns") {
+                                var last = Util.Util.getLast(root);
+                                last.addColumnObj(new Column.Column(keyword));
+                                dataContext = "column_type";
                                 keyword = '';
                             }
-                            else if (maincontext == 'primary') {
+                            else if (mainContext == 'primary') {
                                 //sails handles id creation so there is no need for our tree to have one
                                 //so let remove the primary key constraint
                                 //this is for situations where the primary key is created at the end of the
                                 //table deceleration.
-                                var last = Util_1.Util.getLast(root);
+                                var last = Util.Util.getLast(root);
                                 var tmp = last.getColumnObj();
                                 for (var i = 0; i < tmp.length; i++) {
                                     if (tmp[i].getName() == keyword) {
@@ -113,33 +113,33 @@ var Reader = (function () {
                                     }
                                 }
                             }
-                            else if (maincontext == 'foreign') {
+                            else if (mainContext == 'foreign') {
                                 if (keyword.length == 0)
                                     continue;
                                 foreignKeyName = keyword;
                                 keyword = '';
-                                datacontext = '';
+                                dataContext = '';
                             }
-                            else if (tmpData == "__ac" && maincontext == "references") {
-                                var tmp = Util_1.Util.getLast(root);
+                            else if (tmpData == "__ac" && mainContext == "references") {
+                                var tmp = Util.Util.getLast(root);
                                 if (foreign_root === null)
-                                    foreign_root = new ForeignKey_1.ForeignKey(foreignKeyName, keyword, tmp.getTableName());
+                                    foreign_root = new ForeignKey.ForeignKey(foreignKeyName, keyword, tmp.getTableName());
                                 else {
-                                    var last = Util_1.Util.getLast(foreign_root);
-                                    last.setNext(new ForeignKey_1.ForeignKey(foreignKeyName, keyword, tmp.getTableName()));
+                                    var last = Util.Util.getLast(foreign_root);
+                                    last.setNext(new ForeignKey.ForeignKey(foreignKeyName, keyword, tmp.getTableName()));
                                 }
-                                datacontext = '';
+                                dataContext = '';
                                 tmpData = '';
                             }
                             else {
                                 tmpData = keyword;
                                 keyword = '';
-                                datacontext = '';
+                                dataContext = '';
                             }
                         }
                         else {
                             //init keyword and start collecting data
-                            datacontext = "data_end";
+                            dataContext = "data_end";
                         }
                         break;
                     case ')':
@@ -147,13 +147,13 @@ var Reader = (function () {
                             ignore = false;
                         break;
                     case '(':
-                        if (datacontext == "column_type") {
+                        if (dataContext == "column_type") {
                             ignore = true;
                             continue;
                         }
-                        else if (maincontext == 'primary' || maincontext == 'foreign' || maincontext == 'references')
+                        else if (mainContext == 'primary' || mainContext == 'foreign' || mainContext == 'references')
                             continue;
-                        maincontext = "columns";
+                        mainContext = "columns";
                         keyword = '';
                         break;
                     case '.':
@@ -167,8 +167,8 @@ var Reader = (function () {
                 }
             }
             if (!someDataRead) {
-                Util_1.Util.printTree(root);
-                Util_1.Util.printForeignTree(foreign_root);
+                Util.Util.printTree(root);
+                Util.Util.printForeignTree(foreign_root);
                 return cb(null, root, foreign_root);
             }
         });
